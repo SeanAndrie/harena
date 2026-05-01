5b1eafc30497eb6af9dcaa13f48e4f9cde3437c9
SeanAndrie <gseanandrie@gmail.com>
2026-05-02 02:20:41 +0400

    refactor: zero-out fields explicitly

--- src/harena_destroy.c
+++ src/harena_destroy.c
@@ -6,18 +6,17 @@
 /*   By: sgadinga <sgadinga@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/02 00:55:41 by sgadinga          #+#    #+#             */
-/*   Updated: 2026/05/02 01:09:49 by sgadinga         ###   ########.fr       */
+/*   Updated: 2026/05/02 02:19:30 by sgadinga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <harena.h>
 
-void	harena_destroy(t_harena *h)
+void        harena_destroy(t_harena *h)
 {
-	if (!h || !h->base)
-		return ;
-	free(h->base);
-	h->base = NULL;
+    if (!h || !h->base)
+        return ;
+    free(h->base);
+    ft_memset(h, 0, h->offset);
     h->offset = 0;
-    h->capacity = 0;
 }
